246c44626590da98e497022062845bee778912e6
A. Akbar Hidayat <keripix@gmail.com>
2013-11-26 20:05:08 +0700

    drag event

--- drag/script.js
+++ drag/script.js
@@ -1,10 +1,22 @@
 var items = document.querySelectorAll(".item");
 
-function dragging( el ) {
+function elMulaiDrag( el ) {
+  // jadikan transparan
   this.style.opacity = 0.5;
+}
+
+function elDrag( el ) {
+  this.style.opacity = parseFloat(this.style.opacity) + 0.05;
+
+  if (this.style.opacity >= 1) {
+    this.style.opacity = 0;
+  }
 }
 
 // Memasang event handler untuk proses dragging
 [].forEach.call(items, function( el ) {
-  el.addEventListener("dragstart", dragging, false);
+  // ketika element mulai di drag
+  el.addEventListener("dragstart", elMulaiDrag, false);
+  // ketika element di drag
+  el.addEventListener("drag", elDrag, false);
 });
