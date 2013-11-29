7b599fafd8566f6289b37c27bca2cdff0e2dd91d
A. Akbar Hidayat <keripix@gmail.com>
2013-11-29 20:34:25 +0700

    perbaharui code server

--- drag-n-ajax/upload.php
+++ drag-n-ajax/upload.php
@@ -2,12 +2,16 @@
 
 $uploadedFiles = [];
 
-foreach ($_FILES["file"]["name"] as $key => $name) {
-    $uploadedFiles[] = array(
-        "name" => $_FILES["file"]["name"][$key],
-        "type" => $_FILES["file"]["type"][$key],
-        "size" => $_FILES["file"]["size"][$key]
-    );
+foreach ($_FILES["file"]["error"] as $key => $error) {
+
+    if ($error == UPLOAD_ERR_OK) {
+        $uploadedFiles[] = array(
+            "name" => $_FILES["file"]["name"][$key],
+            "type" => $_FILES["file"]["type"][$key],
+            "size" => $_FILES["file"]["size"][$key]
+        );
+    }
+    
 }
 
 echo json_encode($uploadedFiles);
